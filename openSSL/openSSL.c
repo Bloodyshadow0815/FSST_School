c7ef7d0f267a93817300281e35a013e4f8485a0c
BloodyShadow0815 <bloodyshadow0815@gmail.com>
2021-03-24 23:58:51 +0100

    ooph

--- openSSL/openSSL.c
+++ openSSL/openSSL.c
@@ -22,51 +22,6 @@ void handleErrors()
     abort();
 }
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *ciphertext)
-{
-    EVP_CIPHER_CTX *ctx;
-
-    int len;
-
-    int ciphertext_len;
-
-    /* Create and initialise the context */
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
-
-    /*
-     * Initialise the encryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
-    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
-        handleErrors();
-
-    /*
-     * Provide the message to be encrypted, and obtain the encrypted output.
-     * EVP_EncryptUpdate can be called multiple times if necessary
-     */
-    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-        handleErrors();
-    ciphertext_len = len;
-
-    /*
-     * Finalise the encryption. Further ciphertext bytes may be written at
-     * this stage.
-     */
-    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
-        handleErrors();
-    ciphertext_len += len;
-
-    /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
-
-    return ciphertext_len;
-}
-
 int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
             unsigned char *iv, unsigned char *plaintext)
 {
@@ -94,7 +49,7 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
      * Provide the message to be decrypted, and obtain the plaintext output.
      * EVP_DecryptUpdate can be called multiple times if necessary.
      */
-    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, 32))
         handleErrors();
     plaintext_len = len;
 
@@ -135,22 +90,22 @@ int main (void)
      * ciphertext which may be longer than the plaintext, depending on the
      * algorithm and mode.
      */
-    //unsigned char ciphertext[32]={
-    /*    0xAA, 0xE3, 0x65, 0x27, 0x2C, 0x81, 0x07, 0x8A,
+    unsigned char ciphertext[32]={
+        0xAA, 0xE3, 0x65, 0x27, 0x2C, 0x81, 0x07, 0x8A,
         0xB6, 0x11, 0x6B, 0x36, 0x18, 0x31, 0xD0, 0xF6,
         0xA5, 0xD3, 0xC8, 0x58, 0x7E, 0x94, 0x6B, 0x53,
         0X0B, 0x79, 0x57, 0x54, 0x31, 0x07, 0xF1, 0x5E
     };
-    */
-    unsigned char ciphertext[128];
+    
+  //  unsigned char ciphertext[128];
     /* Buffer for the decrypted text */
     unsigned char decryptedtext[128];
 
     int decryptedtext_len, ciphertext_len;
 
     /* Encrypt the plaintext */
-    ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
-                              ciphertext);
+//    ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
+//                              ciphertext);
     printf("%d\n",ciphertext_len);
     /* Do something useful with the ciphertext here */
     printf("Ciphertext is:\n");
@@ -170,4 +125,5 @@ int main (void)
 
     return 0;
 }
+
 
